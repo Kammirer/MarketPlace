7e188f10c93419b5bc68303ecef5555fa08d0474
Kammirer <kammirer777@gmail.com>
2018-12-27 18:59:40 +0200

    mp_27.12.18_18.59

--- app/js/init.js
+++ app/js/init.js
@@ -1,8 +1,21 @@
-$('.multiple-items').slick({
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    autoplay: true,
-    arrows: false,
-    dots: true
-  });
+// $('.multiple-items').slick({
+//     infinite: true,
+//     slidesToShow: 4,
+//     slidesToScroll: 4,
+//     autoplay: true,
+//     arrows: false,
+//     dots: true
+//   });
+
+// popup
+// let openModalBtn = $('.open-login-modal');
+// openModalBtn.on('click', function () {
+//   $('.modal').bPopup();
+// })
+
+let openModalsBtns = $('[data-modal]');
+openModalsBtns.on('click', function () {
+  let target = $(this).attr('data-target');
+  console.log(target);
+  $('.modal').bPopup();
+})

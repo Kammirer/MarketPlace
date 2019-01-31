12bdfa333d61d1bca93b79b4f56ef50efa5f4333
Kammirer <kammirer777@gmail.com>
2019-01-31 19:07:57 +0200

    mp_31.01.19_19.07

--- app/js/init.js
+++ app/js/init.js
@@ -1,11 +1,11 @@
-$('.multiple-items') .slick({
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    autoplay: true,
-    arrows: false,
-    dots: true
-  });
+// $('.multiple-items') .slick({
+//     infinite: true,
+//     slidesToShow: 4,
+//     slidesToScroll: 4,
+//     autoplay: true,
+//     arrows: false,
+//     dots: true
+//   });
 
 // popup
 // let openModalBtn = $('.open-login-modal');
@@ -18,4 +18,19 @@ openModalsBtns.on('click', function () {
   let target = $(this).attr('data-target');
   console.log(target);
   $('.modal').bPopup();
+})
+
+// Switch all-items cards
+$('#grid-thumbs').on('click', function() {
+  $('.change-icon').removeClass('active');
+  $('#grid-thumbs').addClass('active');
+  $('.all-new-card .row').removeClass('flex-column');
+  $('.card.card-min-items').removeClass('row-card');
+})
+
+$('#grid-list').on('click', function() {
+  $('.change-icon').removeClass('active');
+  $('#grid-list').addClass('active');
+  $('.all-new-card .row').addClass('flex-column');
+  $('.card.card-min-items').addClass('row-card');
 })

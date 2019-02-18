08031baa820aa2aabd21298a430f05ed63d70a7c
Kammirer <kammirer777@gmail.com>
2019-02-18 15:08:14 +0200

    mp_18.02.19_15.08

--- app/js/init.js
+++ app/js/init.js
@@ -21,16 +21,19 @@ openModalsBtns.on('click', function () {
 })
 
 // Switch all-items cards
-$('#grid-thumbs').on('click', function() {
-  $('.change-icon').removeClass('active');
-  $('#grid-thumbs').addClass('active');
-  $('.all-new-card .row').removeClass('flex-column');
-  $('.card.card-min-items').removeClass('row-card');
-})
-
 $('#grid-list').on('click', function() {
   $('.change-icon').removeClass('active');
   $('#grid-list').addClass('active');
   $('.all-new-card .row').addClass('flex-column');
   $('.card.card-min-items').addClass('row-card');
+  $('.all-new-card-right').removeClass('col-lg-4');
+  $('.all-new-card-right').addClass('col-lg-12');
+})
+$('#grid-thumb').on('click', function() {
+  $('.change-icon').removeClass('active');
+  $('#grid-thumb').addClass('active');
+  $('.all-new-card .row').removeClass('flex-column');
+  $('.card.card-min-items').removeClass('row-card');
+  $('.all-new-card-right').removeClass('col-lg-12');
+  $('.all-new-card-right').addClass('col-lg-4');
 })
